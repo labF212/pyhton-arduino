52a69b40796367771dc7fd59181612aeeaed9842
Paulo Galvão <paulo.galvao@estsetubal.ips.pt>
2023-01-20 16:42:17 +0000

    Add files via upload

--- firmata_pysimplegui_analog_in_out.py
+++ firmata_pysimplegui_analog_in_out.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#/usr/bin/env python3
 
 # como correr: sudo /usr/bin/python3 firmata_portas_analogicas_pot_pwd.py
 #https://pypi.org/project/pyFirmata/
@@ -37,11 +37,12 @@ sg.theme('DarkAmber')   # Add a touch of color
 layout = [
             [sg.Text('Potenciómetro AI1:'),
             sg.Text('None',expand_x=True,key='-TEXTAI1-',justification='center',auto_size_text=True)],
-            [sg.ProgressBar(100, orientation='h', size=(50,12), bar_color="grey", key='progressbar_AI1',expand_x=True)],
+            [sg.ProgressBar(100, orientation='h', size=(50,12), key='progressbar_AI1',expand_x=True)],
             [sg.Text('Potenciómetro AI5:'),
             sg.Text('None',expand_x=True,key='-TEXTAI5-',justification='center',auto_size_text=True)],
-            [sg.ProgressBar(100, orientation='h', size=(50,12), bar_color="grey", key='progressbar_AI5',expand_x=True)],
-            [sg.Button('Ok'), sg.Button('Sair')]
+            [sg.ProgressBar(100, orientation='h', size=(50,12), key='progressbar_AI5',expand_x=True)],
+            [sg.Push(),sg.Button((' LED VERDE  '),key="led1"), sg.Button((' LED AMARELO  '),key="led2"), sg.Button((' LED VERMELHO  '),key="led3"),sg.Push()],
+            [sg.Push(),sg.Button('Ok'), sg.Button('Sair'),sg.Push()]
             ]
 
 # Create the Window
@@ -76,8 +77,29 @@ while True:
     print('a5', int(float(RawValue5)*255))
     #time.sleep(1.0)
     digital_write4.write(1)
+    window["led1"].update(button_color='green')
     
-      
+    
+    
+    if  RawValue1<=0.1:
+         window["led2"].update(button_color=sg.theme_button_color_background())
+    else:
+        window["led2"].update(button_color='yellow')
+    
+    
+    if  RawValue5<=0.1:
+         window["led3"].update(button_color=sg.theme_button_color_background()) 
+         
+    else:
+        #https://www.colorhexa.com/8b0000 tint color variation
+        #ffeded,#ffd9d9,#ffc6c6,#ffb2b2,#ff9f9f
+        s= "0000"
+        a=  "D80000" # '#8B0000' '#a50000´
+        b= int(float(RawValue5)*255)+40000
+        # hex(int(a, 16)
+        var_color = hex(int (a,16)+b)
+        print("#"+var_color[2:])
+        window["led3"].update(button_color="#"+var_color[2:]) #'red'
     
     
     if event == sg.WIN_CLOSED or event == 'Sair': # if user closes window or clicks cancel
