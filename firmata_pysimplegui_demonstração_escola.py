60e09751946ae6065ee9a95b7179d0ac14c23012
Paulo Galvão <paulo.galvao@estsetubal.ips.pt>
2023-03-01 15:55:07 +0000

    Add files via upload

--- firmata_pysimplegui_demonstração_escola.py
+++ firmata_pysimplegui_demonstração_escola.py
@@ -23,18 +23,19 @@ RawValue5 = None
 sg.theme('DarkAmber')
 
 layout = [
-    [sg.Text(datetime.datetime.now().strftime(f'{DATE_FORMAT} - {TIME_FORMAT}'), font=('Helvetica', 14), justification='center', key='-DATE_TIME-')],
-    [sg.Text('Sensor Sharp 2Y0A21 F 44'),
+    [sg.Text(datetime.datetime.now().strftime(f'{DATE_FORMAT}'), font=('Helvetica', 14), key='-DATE-'),sg.Push(),
+    sg.Text(datetime.datetime.now().strftime(f'{TIME_FORMAT}'), font=('Helvetica', 14), key='-TIME-')],
+    [sg.Text('Sensor Radio-Frequência Infravermelho - Sharp  2Y0A21 F 44 (10cm a 80cm)'),
      sg.Text('None', expand_x=True, key='-TEXTAI1-', justification='right', auto_size_text=True)],
     [sg.ProgressBar(100, orientation='h', size=(50, 12), key='-progressbar_AI1-', expand_x=True)],
-    [sg.Text('Sensor DFRobot'),
+    [sg.Text('Sensor Ultrassónico - DFRobot SEN0307 (2cm a 500cm)'),
      sg.Text('None', expand_x=True, key='-TEXTAI5-', justification='right', auto_size_text=True)],
     [sg.ProgressBar(100, orientation='h', size=(50, 12), key='-progressbar_AI5-', expand_x=True)],
-    [sg.Push(), sg.Button('Ok'), sg.Button('Sair'), sg.Button('', key='alarme', button_color=('white', 'green')),
+    [sg.Push(), sg.Button('Sobre'), sg.Button('Sair'), sg.Button('', key='alarme', button_color=('white', 'green')),
      sg.Push()]
 ]
 
-window = sg.Window('Medição de Distância (cm)', layout)
+window = sg.Window('Medição de Distância (cm)', layout,resizable=True)
 
 while True:
     event, values = window.read(timeout=1000)
@@ -49,17 +50,23 @@ while True:
         board.exit()
         break
 
-    if event == 'Ok':
+    if event == 'Sobre':
         sg.popup('Programa escrito em Python', 'GUI - PysimpleGui', 'Pyfirmata', title='Informação')
 
     progressbar_AI1.UpdateBar(RawValue1 * 100)
     progressbar_AI5.UpdateBar(RawValue5 * 100)
-    window['-TEXTAI1-'].update(str(round(RawValue1 * 5 * 100, 2)) + ' cm')
-    window['-TEXTAI5-'].update(str(round(RawValue5 * 5 * 100, 2)) + ' cm')
+    
+    valor_medido_sensorAI1= round((6762/(RawValue1 * 1023-9)-4),2) #em cm
+    valor_medido_sensorAI5= round((RawValue5 * 5 * 100),2) #em cm
+    
+    window['-TEXTAI1-'].update(str(valor_medido_sensorAI1) + ' cm')
+    #window['-TEXTAI1-'].update(str(round(RawValue1 * 5 * 100, 2)) + ' cm')
+    #cmValue = (6762/(sensorValue-9))-4
+    window['-TEXTAI5-'].update(str(valor_medido_sensorAI5) + ' cm')
 
     # verifica o status do LED 13 e atualiza o botão de alarme
     # Acender LED no pino digital 13 se o valor lido do pino analógico A5 for superior a 0.8
-    if RawValue5 > 0.8:
+    if valor_medido_sensorAI5 < 10:
         digital_write13.write(1)
         window['alarme'].update('Alarme Ligado', button_color=('white', 'red'))
     else:
@@ -70,6 +77,7 @@ while True:
     
     # atualiza a hora atual e a data
     now = datetime.datetime.now()
-    window['-DATE_TIME-'].update(now.strftime(f'{DATE_FORMAT} - {TIME_FORMAT}'))
+    window['-DATE-'].update(now.strftime(f'{DATE_FORMAT}'))
+    window['-TIME-'].update(now.strftime(f'{TIME_FORMAT}'))
     
 window.close()
